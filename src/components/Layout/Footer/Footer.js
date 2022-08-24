76e5a5d30e10b2ff7329a3c949a09f14296356b9
programmer686 <105795650+programmer686@users.noreply.github.com>
2022-08-23 19:50:08 -0500

    my changes

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -16,9 +16,9 @@ function Footer() {
     <div className={styles.main}>
       <p className={styles.findme}>Find me anywhere at <span>programmer686</span></p>
       <div>
-      <a href="https://www.facebook.com/profile.php?id=100084364404347" target="_blank" rel="noreferrer"><i className='fa fa-github fa-3x icons'></i></a>
+      <a href="https://github.com/programmer686" target="_blank" rel="noreferrer"><i className='fa fa-github fa-3x icons'></i></a>
       <button onClick={() => copyClipboard()}><i className='fa fa-envelope fa-3x icons'></i> </button>
-      <a href="https://github.com/programmer686" target="_blank" rel="noreferrer"> <i className='fa fa-facebook fa-3x icons'></i>  </a>
+      <a href="https://www.facebook.com/profile.php?id=100084364404347" target="_blank" rel="noreferrer"> <i className='fa fa-facebook fa-3x icons'></i>  </a>
       </div>
     </div>
     </html>
